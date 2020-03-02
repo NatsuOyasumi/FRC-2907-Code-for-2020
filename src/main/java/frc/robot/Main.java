65e9078962f26c774ecb94ee95b8127c7c8ad93e
NatsuOyasumi <47455002+NatsuOyasumi@users.noreply.github.com>
2020-03-02 15:16:32 -0800

    Removing bogus comment
    Removing random comments

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -13,9 +13,8 @@ import edu.wpi.first.wpilibj.RobotBase;
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
  * call.
- *
- */ //xitfffffffffffe
- ///exxxxxdgdgdgdghfhfhfhfhfhfhfhfhfhf
+ */
+
 public final class Main {
   private Main() {
   }
