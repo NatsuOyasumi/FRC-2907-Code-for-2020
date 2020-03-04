bfcb5a3c7383fe0622f650846aab4caeec5720f1
NatsuOyasumi <47455002+NatsuOyasumi@users.noreply.github.com>
2020-03-03 17:57:51 -0800

    encoder
    trying to get encoders to put stuff in the dashboard for autonomouoususus (can you tell i dont know how to spell autonomos?)
    
    basically, i did this:
    1. copied and pasted some code into a new subsystem
    2. made the motors public static
    3. got it to (hopefully) show us the data on smartdashboard

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -16,6 +16,8 @@ import frc.robot.commands.autonomous.AutoCommand19;
 import frc.robot.commands.autonomous.AutoCommand2;
 import frc.robot.commands.autonomous.AutoCommand3;
 import frc.robot.commands.autonomous.AutoCommand4;
+import frc.robot.subsystems.ArcadeDrive;
+import frc.robot.subsystems.SRXMagEncoder_Relative;
 //example change
 
 public class Autonomous {
@@ -31,6 +33,11 @@ public class Autonomous {
 
     SendableChooser<Command> chooser = new SendableChooser<Command>();
 
+    private SRXMagEncoder_Relative encoderLeft = new SRXMagEncoder_Relative(ArcadeDrive.leftMaster);
+    private SRXMagEncoder_Relative encoderRight = new SRXMagEncoder_Relative(ArcadeDrive.rightMaster);
+    //for encoder stuff, dont touch pls
+    //we also made the talon motors public static to make this work
+
     // Really don't know if this is needed but it worked so yeah.
     public void scheduleAuto() {
 
@@ -45,7 +52,6 @@ public class Autonomous {
 
     // Chooser in smart dashboard for autonomous.
     public void getAutonomous() {
-
         chooser.setDefaultOption("Defaut Auto", m_autoCommand3);
         chooser.addOption("Forward 0.5", m_autoCommand0);
         chooser.addOption("Backward 0.5", m_autoCommand1);
@@ -54,7 +60,15 @@ public class Autonomous {
         chooser.addOption("Spin", m_autoCommand4);
         chooser.addOption("Run slow", m_autoCommand19);
         SmartDashboard.putData("AutoMode", chooser);
-
+       // try {
+        SmartDashboard.putNumber("Encoder Stuff (the right one(EncoderRight))", encoderRight.getPosition());
+        SmartDashboard.putNumber("Encoder Stuff (the left one(EncoderLeft))", encoderLeft.getPosition());
+       //SmartDashboard.putNumber(key, value)
+        //} catch(IllegalArgumentException e) {
+          //  e.printStackTrace();
+            //It will try to do SmartDashboard.putData(...); If it fails for some reason, it will
+            //catch the error and print out the error.
+       // }
     }
 
     // On autonomous init start picked.
